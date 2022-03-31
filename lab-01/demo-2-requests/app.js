1c58fc2a0f1de418979a7edc9f18b509f30b97b0
Jaimie Jin <jinjaimie12@gmail.com>
2022-03-31 15:30:58 -0700

    lab 1

--- lab-01/demo-2-requests/app.js
+++ lab-01/demo-2-requests/app.js
@@ -1,3 +1,14 @@
 // TO DO: Get the path of the url passed in
 // TO DO: Get the query parameters of the url passed in
 // TO DO: Get the url of the request
+const express = require('express');
+const app = express();
+
+app.get('*', (req, res) => {
+    console.log(req.url);
+    res.send('you request the url: ' + req.url);
+})
+
+app.listen(3000, () => {
+    console.log('example running on port 3000')
+})
