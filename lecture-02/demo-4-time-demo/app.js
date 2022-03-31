29cb06cca7f96e57cdcebd1db2ac0b4273cee03f
kyle thayer <kyle.thayer@gmail.com>
2022-03-31 16:55:54 -0700

    updated demo 4 to imports

--- lecture-02/demo-4-time-demo/app.js
+++ lecture-02/demo-4-time-demo/app.js
@@ -1,16 +1,16 @@
-const fs = require('fs').promises
-const express = require('express')
+import fs from 'fs'
+import express from 'express'
 const app = express()
 
 app.get('/', async (req, res) => {
   res.type('html')
-  let fileContents = await fs.readFile("index.html")
+  let fileContents = await fs.promises.readFile("index.html")
   res.send(fileContents)
 })
 
 app.get('/style.css', async (req, res) => {
   res.type('css')
-  let fileContents = await fs.readFile("style.css")
+  let fileContents = await fs.promises.readFile("style.css")
   res.send(fileContents)
 })
 
