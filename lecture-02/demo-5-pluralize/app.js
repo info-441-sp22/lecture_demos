b9f4f8537e15eb22ac1ebe0a0816edaa9baf8bad
kyle thayer <kyle.thayer@gmail.com>
2022-04-01 10:12:17 -0700

    fixed fs promises import

--- lecture-02/demo-5-pluralize/app.js
+++ lecture-02/demo-5-pluralize/app.js
@@ -1,23 +1,23 @@
-import fs from 'fs'
+import { promises as fs } from 'fs';
 import express from 'express'
 import pluralize from 'pluralize'
 const app = express()
 
 app.get('/', async (req, res) => {
   res.type('html')
-  let fileContents = await fs.promises.readFile("index.html")
+  let fileContents = await fs.readFile("index.html")
   res.send(fileContents)
 })
 
 app.get('/style.css', async (req, res) => {
   res.type('css')
-  let fileContents = await fs.promises.readFile("style.css")
+  let fileContents = await fs.readFile("style.css")
   res.send(fileContents)
 })
 
 app.get('/index.js', async (req, res) => {
   res.type('js')
-  let fileContents = await fs.promises.readFile("index.js")
+  let fileContents = await fs.readFile("index.js")
   res.send(fileContents)
 })
 
