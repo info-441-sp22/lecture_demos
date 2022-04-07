c8dfacb2fd4f827454e583b25c00e5e93d83dc04
kyle thayer <kyle.thayer@gmail.com>
2022-04-07 16:26:26 -0700

    finished iterator demo

--- lecture-04/demo-1-nodejs-tricks/app.js
+++ lecture-04/demo-1-nodejs-tricks/app.js
@@ -1,3 +1,5 @@
+//Template strings demo
+
 let title = "This page"
 let subtitle = "this is an example page"
 let description = undefined
@@ -38,3 +40,53 @@ let html3 = `
   </body>
 </html>`
 console.log(html3)
+
+
+// Iterators Demo
+let arr = [
+    "first name : Kyle",
+    "last name : Thayer",
+    "age : 38",
+    "glasses : yes"
+]
+
+// "forEach" runs a function on each item in an object/array
+// we use it to extract the values into an object
+let values = {}
+arr.forEach(item => {
+    let split_item = item.split(" : ")
+    values[split_item[0]] = split_item[1]
+})
+console.log(values)
+
+// "map" creates a modified version of an array
+//    it makes a new array, where the result is running a function
+//    item in the old array
+
+// "map" demo 1: replace the ":" with "=" in the original array (arr)
+let modifiedArr = arr.map(item =>{
+    return item.replace(":", "=")
+})
+console.log(modifiedArr)
+
+// "map" demo 2: get just the first part of the items in the array
+let modifiedArr2 = arr.map(item => {
+    return item.split(" : ")[0]
+})
+console.log(modifiedArr2)
+
+// Note: with "map" you could take an array of values, and 
+// return an array of html that has those values in it
+
+// "filter" it goes through an array and makes a new array 
+// with only some of the old items. You provide "filter" with a function
+// and it keeps the items where the function returns true, and tosses out
+// the items where the function returns false.
+let filteredArr = arr.filter(item => {
+    if(item.includes("name")) {
+        return true
+    } else {
+        return false
+    }
+})
+console.log(filteredArr)
