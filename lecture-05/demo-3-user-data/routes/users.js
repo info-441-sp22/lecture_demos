cb5e8ae3e6b8016a45ed068d40c70f7ce846a022
kyle thayer <kyle.thayer@gmail.com>
2022-04-12 15:50:45 -0700

    finished user info demo

--- lecture-05/demo-3-user-data/routes/users.js
+++ lecture-05/demo-3-user-data/routes/users.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import {promises as fs} from "fs";
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* POST users listing. */
+router.post('/addUserData', async function(req, res, next) {
+  console.log(req.body)
+  
+  await fs.writeFile("data/userData.json", JSON.stringify(req.body))
+
+  res.send('success');
 });
+
+/* GET users listing. */
+router.get('/getUserData', async function(req, res, next) {
+  let userInfo = await fs.readFile("data/userData.json")
+  res.type("json")
+  res.send(userInfo);
+});
+
 
 export default router;
