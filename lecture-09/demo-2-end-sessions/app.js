c9ad3ccf0f9951fcfc701c6f63f73e2f5d6fca7b
kyle thayer <kyle.thayer@gmail.com>
2022-04-25 21:36:23 -0700

    finish session demo

--- lecture-09/demo-2-end-sessions/app.js
+++ lecture-09/demo-2-end-sessions/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import sessions from 'express-session'
 
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
@@ -18,6 +19,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+const oneDay = 1000 * 60 * 60 * 24
+app.use(sessions({
+    secret: "this is some secret key I am making up5ew4e[9umvecwljcl'sa",
+    saveUninitialized: true,
+    cookie: {maxAge: oneDay},
+    resave: false
+}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- lecture-09/demo-2-end-sessions/routes/users.js
+++ lecture-09/demo-2-end-sessions/routes/users.js
@@ -3,21 +3,44 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  let session = req.session
+  if(session.userid){
+    res.send('respond with a resource for the user: ' + session.userid);
+  } else {
+    res.send('Error: You must be logged in to see this information')
+  }
+  
 });
 
 router.post("/login", function(req, res, next) {
+  let session = req.session
+  console.log("session at start of login:", session)
+
+  if(session.userid){
+    res.send("Error: You are already logged in as " + session.userid)
+    return
+  }
+
   //check username and password
   if(req.body.username == "kylethayer" && req.body.password == "asdasd"){
-    //TODO: start a session
+    //start a session
+    session.userid = "kylethayer"
+    console.log("session after login:", session)
     res.send("you logged in")
-  } else{
+  } else if(req.body.username == "anotheruser" && req.body.password == "pwd"){
+    // start a session
+    session.userid = "anotheruser"
+    console.log("session after login:", session)
+    res.send("you logged in")
+  }else{
     //not start session
+    console.log("session after failed login:", session)
     res.send("wrong login info")
   }
 
 router.post("/logout", function(req, res, next) {
-  res.send("I should probably do something to log you out")
+  req.session.destroy()
+  res.send("you are logged out")
 })
   
   
