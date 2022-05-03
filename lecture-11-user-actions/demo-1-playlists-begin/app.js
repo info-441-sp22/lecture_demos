90710df09e6c8dc513127ea605cbe7444747b217
kyle thayer <kyle.thayer@gmail.com>
2022-05-03 12:33:35 -0700

    start lecture 11 user actions playlist demo

---
+++ lecture-11-user-actions/demo-1-playlists-begin/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import indexRouter from './routes/index.js';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+export default app;
