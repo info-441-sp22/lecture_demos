27f24f88b942f9279b46c3d614eacc4fac1e2565
kyle thayer <kyle.thayer@gmail.com>
2022-05-05 11:30:20 -0700

    start websocket demo

---
+++ lecture-12-websockets/demo-1-websockets-finish/app.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + "/index.html")
+})
+
+app.listen(3000, () => {
+    console.log("Example app listening at http://localhost:3000")
+})
